0113763b51055433961fc45b3b300118836403e0
MaelleT <maelle.taurand@ac-nantes.fr>
2017-01-19 21:30:02 +0100

    Le code retourné est dans une page index.html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var http = require('http'); //import de la bibliothèque http
 
+var fs = require('fs');
 //création du serveur avec en paramètre une fonction qui est exécutée quand un visiteur
 // se connecte sur le site : il envoie alors une request au serveur
 var server = http.createServer();
@@ -11,20 +12,16 @@ server.on('request',function(req, res) {
 	  //récupération d'informations sur la requête :
 	  var userAgent= req.headers['user-agent'];
       console.log('il y a eu une requête. Le client web utilisé est '+ userAgent );
-	
-	  res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
-	  res.write('<!DOCTYPE html>'+
-      '<html>'+
-      '    <head>'+
-      '        <meta charset="utf-8" />'+
-      '        <title>Ma page Node.js !</title>'+
-      '    </head>'+ 
-      '    <body>'+
-      '     	<h1>Hello <strong>WORLD</strong> !</h1>'+
-      '    </body>'+
-      '</html>');
-      res.end();
-	  res.end('Hello world');
+
+	  
+	  fs.readFile('index.html','utf-8',function(err,data){
+	 	if (err) throw err;
+	  	res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+		res.write(data);
+		res.end();
+	  
+	  });
+  
 });
 
 try {
