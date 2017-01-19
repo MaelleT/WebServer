9229715affdf18d4b37c0aecc3680cbfec2042cf
MaelleT <maelle.taurand@ac-nantes.fr>
2017-01-19 13:08:09 +0100

    Creation et démarrage du serveur

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+//console.log('Hello World');
+
+var http = require('http'); //import de la bibliothèque http
+
+//création du serveur avec en paramètre une fonction qui est exécutée quand un visiteur
+// se connecte sur le site : il envoie alors une request au serveur
+var server = http.createServer();
+
+server.on('request',function(req, res) {
+	
+      console.log('il y a eu une requête');
+});
+
+try {
+  server.listen(8081); //démarrage du serveur sur le port 8080
+} catch (err) {
+	console.error('there was an error starting web server !', err);
+}
