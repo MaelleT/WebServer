78bffbb794650af66c37b1b1a03ce2a774b1a400
MaelleT <maelle.taurand@ac-nantes.fr>
2017-01-19 20:10:34 +0100

    Mise en forme de la response

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ var server = http.createServer();
 server.on('request',function(req, res) {
 	
       console.log('il y a eu une requête');
+	  res.writeHead(200);
+	  res.end('Hello !! comment ça va ?');
 });
 
 try {
-  server.listen(8081); //démarrage du serveur sur le port 8080
+  server.listen(8081); //démarrage du serveur sur le port 8081
 } catch (err) {
-	console.error('there was an error starting web server !', err);
+	console.error(' Erreur de démarrage du serveur!', err);
 }
