8e80d0b116f04476b313158ab7d1bea8cdac8cb9
MaelleT <maelle.taurand@ac-nantes.fr>
2017-01-30 22:04:26 +0100

    envoi de la requête

--- weather.js
+++ weather.js
@@ -1,1 +1,13 @@
 // Application  : manipulation du résultat d'une requête
+// site de données météo : openweathermap.org
+
+// récupération des informations sur la météo Nantaise
+var http = require('http');
+
+var request = http.get("http://api.openweathermap.org/data/2.5/weather?q=Nantes&appid=1a02c3d069d60c5db4f277a894120c6d",
+					   //appid correspond à une clé suite à une authentification sur le site 
+					   function (res){
+									 
+							console.log(res.statusCode);
+	
+});
